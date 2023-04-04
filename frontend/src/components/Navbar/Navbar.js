8409881a347e23f169d631441d702f34da956e99
Hrutvik66 <hrutvikmalshikare66@gmail.com>
2023-04-05 00:40:59 +0530

    Some minor changes

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav
+        className="navbar navbar-expand-lg sticky-top"
+        style={{ backgroundColor: "white" }}
+      >
+        <NavLink to="/" className="navbar-brand" aria-current="page">
+          <h5 className="name">Retsurant</h5>
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink to="/userMenu" className="nav-link">
+                Menu
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/gallery" className="nav-link">
+                Gallery
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/user/Home.js
+++ frontend/src/pages/user/Home.js
@@ -1,21 +1,17 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import Footer from '../../components/Footer'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer";
 
 const Home = () => {
   return (
     <>
-      <Navbar/>
-     
-     <div style={{margin:'15px'}}>
-      
-     
-    </div>
+      <Navbar />
 
-     {/* <Footer/> */}
+      <div style={{ margin: "15px" }}></div>
 
+      {/* <Footer/> */}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
